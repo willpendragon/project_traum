32937e8112d3215dbd75a7d994f1bb454ac66fb7
moonchld <degregori.guglielmo@gmail.com>
2023-09-03 16:45:28 +0200

    Character model, enemy model, animations
    Added Character Pixel Art Model + walk/idle animations
    Added Enemy Pixel Art Model + walk animation
    Cleaned Art assets folder

--- Assets/Code/Scripts/FramerateLimiter.cs
+++ Assets/Code/Scripts/FramerateLimiter.cs
@@ -11,7 +11,7 @@ public class FramerateLimiter : MonoBehaviour
     }
     void Start()
     {
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 25;
     }
 
     // Update is called once per frame
